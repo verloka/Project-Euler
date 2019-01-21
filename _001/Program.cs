94ce78fdd1cf1d772290cc7f056ecca65833b673
Verloka Vadim <v.verloka@softpiua.com>
2019-01-21 13:33:08 +0200

    Add #001 problem.

---
+++ _001/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace _001
+{
+    class Program
+    {
+        static long GetAnswer(int top)
+        {
+            long ans = 0;
+
+            //create array with nuimbers and fill him
+            long[] numbers = new long[top];
+            for (int i = 0; i < top; i++)
+                numbers[i] = i;
+
+            var coll = (from item in numbers
+                        where item % 3 == 0 || item % 5 == 0
+                        select item);
+
+            ans = coll.Sum();
+
+            return ans;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("BProject Euler. Verloka Vadim, 2019");
+            Console.WriteLine(new string('-', 50));
+
+
+            Console.Write("Enter top: ");
+            bool result = int.TryParse(Console.ReadLine(), out int top);
+
+            if (result)
+                Console.WriteLine($"Answer: {GetAnswer(top)}");
+            else
+                Console.WriteLine("Wrong 'top' number!");
+
+            Console.ReadLine();
+        }
+    }
+}
